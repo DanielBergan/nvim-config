ef654140585179adfa67be80f64fc2e9f6937a73
DanielBergan <daniel.bergan86@gmail.com>
2025-01-09 21:24:43 -0800

    added more keymaps

--- after/ftplugin/java.lua
+++ after/ftplugin/java.lua
@@ -1,18 +1,18 @@
 local jdtls = require('jdtls')
 
-local root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw', 'pom.xml'}, { upward = true })[1])
+local root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw', 'pom.xml' }, { upward = true })[1])
 
 -- Create data directory if it doesn't exist
 local data_dir = vim.fn.expand('~/.cache/jdtls/workspace/')
 vim.fn.mkdir(data_dir, "p")
 
 local config = {
-    cmd = {
-        'jdtls',
-        '--data',  -- Changed from --jvm-arg=-data to --data
-        data_dir .. vim.fn.fnamemodify(root_dir, ':p:h:t')
-    },
-    root_dir = root_dir,
+	cmd = {
+		'jdtls',
+		'--data', -- Changed from --jvm-arg=-data to --data
+		data_dir .. vim.fn.fnamemodify(root_dir, ':p:h:t')
+	},
+	root_dir = root_dir,
 }
 
 jdtls.start_or_attach(config)
--- init.lua
+++ init.lua
@@ -7,14 +7,21 @@ vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
 
+-- window navigation
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
 -- Highlight when yanking (copying) text
 -- Try it with 'yap' in normal mode
 -- See ':help vim.highlight.on_yank()'
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	desc = 'Highlight when yanking (copying) text',
+	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
-
